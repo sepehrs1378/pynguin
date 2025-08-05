64cd8ca31a3338683f8e330a255f0c9ea219c342
Sepehr Safari <sepehr.safari8731@gmail.com>
2025-08-05 20:50:22 +0330

    Add simple to-test module

---
+++ code_to_test/modules/simple.py
@@ -0,0 +1,9 @@
+def check_triangle(a: int, b: int, c: int) -> bool:
+    if a >= b + c:
+        return False
+    elif b >= a + c:
+        return False
+    elif c >= a + b:
+        return False
+    else:
+        return True
