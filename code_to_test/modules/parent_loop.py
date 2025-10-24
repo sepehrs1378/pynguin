1fc2b7594a5cfdca5007aba8ba1b0ae16ccd0d94
Sepehr Safari <sepehr.safari8731@gmail.com>
2025-10-24 19:15:37 +0330

    update code to test

--- code_to_test/modules/parent_loop.py
+++ code_to_test/modules/parent_loop.py
@@ -1,34 +1,19 @@
-# def f1(x: int):
-#     a = x + 2
-#     if x > 0:
-#         return 1
-#     else:
-#         f2()
-#         return -1
+def f1(x: int) -> int:
+    i = 0
+    while x > 1:
+        # x //= 2
+        # i += 1
+        if i >= 4:
+            i = 4
+    return i
 
 
-# def f2(x: int):
-#     b = x - 2
-#     if x > 0:
-#         f1()
-#         return 1
-#     else:
-#         return -1
-
-
-def f1(x: int):
-    x = x * 2
-    x = x / 2
-    if x > 0:
-        return 1
-    else:
-        return -1
-
-
-def f2(x: int):
-    f1()
-    x = x + 2
-    if x > 3:
-        return 1
-    else:
-        return -1
+# def f2(x: int) -> int:
+#     y = x * 2
+#     res = 0
+#     while res != 4:
+#         y *= 2
+#         res = f1(y)
+#         if res == 4:
+#             return 313
+#     return res
